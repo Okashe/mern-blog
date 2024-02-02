7b58d3a6313c9bb1d732e93ed20c0e401383e3f4
Okashe <okashabdi88@gmail.com>
2024-02-02 00:57:35 -0600

    setting up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Projects from './pages/Projects'
 
 function App() {
   
   return (
-    <div className="text-6xl text-blue-400 font-extrabold">
-      <h1>Hello There!</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+
+
+const Home = () => {
+  return (
+    <div className="text-7xl">Home</div>
+  )
+}
+
+export default Home
