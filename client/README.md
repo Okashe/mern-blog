db212cfedc370222c264bcc1527f251619505f61
Okashe <okashabdi88@gmail.com>
2024-02-02 01:47:40 -0600

    create header component

--- client/README.md
+++ client/README.md
@@ -8,3 +8,5 @@ Currently, two official plugins are available:
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 
 install react-router-dom to enbale routing between pages.
+
+use flowbite for creating dynamic header
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import About from './pages/About'
 import Projects from './pages/Projects'
+import Header from './components/Header'
 
 function App() {
   
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/about' element={<About/>}/>
