9aedc6eda67771ddc6a0de7549276337a62c2a6c
Okashe <okashabdi88@gmail.com>
2024-02-03 01:12:10 -0600

    signin api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import {errorHandler} from '../utils/error.js'
 
 export const signUp = async(req, res, next) => {
@@ -8,7 +9,7 @@ export const signUp = async(req, res, next) => {
 
     if(!username ||!email ||!password || username==='' ||email===''||password===''){
         // return res.status(400).json({message:"All fields are required"})
-        next(errorHandler(400, 'All fields required'))
+        return next(errorHandler(400, 'All fields required'))
     }
 
     const hashedPassword = bcryptjs.hashSync(password, 10)
@@ -28,4 +29,43 @@ export const signUp = async(req, res, next) => {
         // res.status(500).json({message:error.message})
         next(error)
    }
+}
+
+export const signIn = async(req, res, next) => {
+    
+    const {email, password} =  req.body
+    if(!email ||!password ||email===''||password===''){
+        // return res.status(400).json({message:"All fields are required"})
+        next(errorHandler(400, 'All fields required'))
+    }
+
+    try{
+        const validUser = await  User.findOne({email})
+
+        if(!validUser){
+         return next(errorHandler(404, 'User not found'))
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+
+        if(!validPassword){
+           return next(errorHandler(400, 'Invalid password'))
+        }
+
+      
+
+        const token = jwt.sign(
+            {id: validUser._id}, process.env.JWT_SECRET
+         )
+
+         const {password:pass , ...rest} = validUser._doc
+         
+         res.status(200).cookie('access_token', token, {
+            httpOnly: true
+         }).json(rest)
+    }catch(error){
+        next(error)
+    }
+
+   
 }
